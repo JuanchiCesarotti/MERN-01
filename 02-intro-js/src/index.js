030f76c81f516304ab750470ae1ca282b1c1937d
Juanchi <jcesarotti.dev@gmail.com>
2023-07-25 14:47:06 -0300

    Operador Ternario

---
+++ 02-intro-js/src/index.js
@@ -0,0 +1,21 @@
+
+
+const activo = false 
+
+
+
+// let mensaje = ''
+
+// if(!activo){
+//     mensaje = 'activo'
+// }
+// else{
+//     mensaje = 'Inactivo'
+// }
+
+// console.log(mensaje)
+
+
+const mensaje = ( activo ) ? 'Activo' : 'Inactivo'
+
+console.log(mensaje)
