8dcb29845855fcdcd7f765ace6953684e3c4225a
Juanchi <jcesarotti.dev@gmail.com>
2023-07-25 16:28:02 -0300

    React Firsdt App

---
+++ 03-counter_app-vite/counter-app/src/FirstApp.jsx
@@ -0,0 +1,16 @@
+// export default function App () {
+//     return (<h1>Hola Mundo</h1>)
+// }
+
+
+// import React from 'react'
+
+const FirstApp = () => {
+  return (
+    <div>
+      <h1>Default App</h1>
+    </div>
+  )
+}
+
+export default FirstApp
--- 03-counter_app-vite/counter-app/src/main.jsx
+++ 03-counter_app-vite/counter-app/src/main.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import reactDOM from 'react-dom/client'
 
 
-function App () {
-    return (<h1>Hola Mundo</h1>)
-}
+// MY IMPORTS 
+import FirstApp from './FirstApp'
 
 
 reactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App></App>
+        <FirstApp></FirstApp>
     </React.StrictMode>
 )
