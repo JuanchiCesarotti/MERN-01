68b88310a37a6c0ea0c6f6e12d6a1090052693e1
Juanchi <jcesarotti.dev@gmail.com>
2023-07-25 16:52:25 -0300

    New getResaults Function

--- 03-counter_app-vite/counter-app/src/DefaultApp.jsx
+++ 03-counter_app-vite/counter-app/src/DefaultApp.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+const newMessage = 'Juanchi';
+
+const JsonTest = {
+    nombre: 'Juuanchi',
+    apellido: 'Cesarotti'
+}
+
+const getResault = () => {
+    return 4+4
+}
 
 
 export default function DefaultApp() {
-  return (
+
+    return (
     <>
     <h1>Default App</h1>
-    <p>Juanchi</p>
+    <p>{getResault()}</p>
+    {/* <p>{JSON.stringify(JsonTest)}</p> */}
     </>
   )
 }
